a4239247eb3ff97b167be44f08883ca651432cac
mktopuz <mktopuz@yahoo.com>
2017-09-18 16:30:29 -0500

    Update customTests.R

--- 4.3-Grouping_and_Chaining_with_dplyr/customTests.R
+++ 4.3-Grouping_and_Chaining_with_dplyr/customTests.R
@@ -1,53 +1,130 @@
-# AUTO_DETECT_NEWVAR <- FALSE
+AUTO_DETECT_NEWVAR <- FALSE
 
-notify <- function() {
-  e <- get("e", parent.frame())
-  if(e$val == "No") return(TRUE)
-  
-  good <- FALSE
-  while(!good) {
-    # Get info
-    name <- readline_clean("What is your full name? ")
-    address <- readline_clean("What is the email address of the person you'd like to notify? ")
-    
-    # Repeat back to them
-    message("\nDoes everything look good?\n")
-    message("Your name: ", name, "\n", "Send to: ", address)
-    
-    yn <- select.list(c("Yes", "No"), graphics = FALSE)
-    if(yn == "Yes") good <- TRUE
+script_results_identical <- function(result_name) {
+  # Get e
+  e <- get('e', parent.frame())
+  # Get user's result from global
+  if(exists(result_name, globalenv())) {
+    user_res <- get(result_name, globalenv())
+  } else {
+    return(FALSE)
   }
+  # Source correct result in new env and get result
+  tempenv <- new.env()
+  # Capture output to avoid double printing
+  temp <- capture.output(
+    local(
+      try(
+        source(e$correct_script_temp_path, local = TRUE),
+        silent = TRUE
+      ),
+      envir = tempenv
+    )
+  )
+  correct_res <- get(result_name, tempenv)
+  # Compare results
+  identical(user_res, correct_res)
+}
+
+# Multiple expression version of expr_creates_var
+multi_expr_creates_var <- function(correctName=NULL){
+  e <- get("e", parent.frame())
+  # TODO: Eventually make auto-detection of new variables an option.
+  # Currently it can be set in customTests.R
+  delta <- if(!customTests$AUTO_DETECT_NEWVAR){
+    safeEval(e$expr, e)
+  } else {
+    e$delta
+  }
+  if(is.null(correctName)){
+    passed <- length(delta) > 0
+  } else {
+    passed <- correctName %in% names(delta)
+  }
+  passed <- isTRUE(passed)
+  if(passed){
+    e$newVar <- e$val
+    e$newVarName <- names(delta)[1]
+    e$delta <- mergeLists(delta, e$delta)
+  }
+  return(passed)
+}
+
+# Check that the output/value produced by a script is correct
+script_vals_identical <- function() {
+  # Get e
+  e <- get('e', parent.frame())
+  # Get value produced from user's script
+  user_val <- capture.output(
+    local(
+      try(
+        # Must use eval-parse combo, not source, if we don't force user
+        # to print result
+        eval(e$expr),
+        silent = TRUE
+      )
+    )
+  )
+  # Get value produced from correct script
+  correct_val <- capture.output(
+    local(
+      try(
+        # Must use eval-parse combo, not source, if we don't force user
+        # to print result
+        eval(parse(file = e$correct_script_temp_path)),
+        silent = TRUE
+      )
+    )
+  )
+  # Compare values
+  identical(user_val, correct_val)
+}
+
+# Get the swirl state
+getState <- function(){
+  # Whenever swirl is running, its callback is at the top of its call stack.
+  # Swirl's state, named e, is stored in the environment of the callback.
+  environment(sys.function(1))$e
+}
+
+# Get the value which a user either entered directly or was computed
+# by the command he or she entered.
+getVal <- function(){
+  getState()$val
+}
+
+# Get the last expression which the user entered at the R console.
+getExpr <- function(){
+  getState()$expr
+}
+
+coursera_on_demand <- function(){
+  selection <- getState()$val
+  if(selection == "Yes"){
+    email <- readline("What is your email address? ")
+    token <- readline("What is your assignment token? ")
+    
+    payload <- sprintf('{  
+      "assignmentKey": "bA2_vK8hEeWK-gr2JZre1Q",
+      "submitterEmail": "%s",  
+      "secret": "%s",  
+      "parts": {  
+        "UrEnQ": {  
+          "output": "correct"  
+        }  
+      }  
+    }', email, token)
+    url <- 'https://www.coursera.org/api/onDemandProgrammingScriptSubmissions.v1'
   
-  # Get course and lesson names
-  course_name <- attr(e$les, "course_name")
-  lesson_name <- attr(e$les, "lesson_name")
-  
-  subject <- paste(name, "just completed", course_name, "-", lesson_name)
-  body = ""
-  
-  # Send email
-  swirl:::email(address, subject, body)
-  
-  hrule()
-  message("I just tried to create a new email with the following info:\n")
-  message("To: ", address)
-  message("Subject: ", subject)
-  message("Body: <empty>")
-  
-  message("\nIf it didn't work, you can send the same email manually.")
-  hrule()
-  
-  # Return TRUE to satisfy swirl and return to course menu
+    respone <- httr::POST(url, body = payload)
+    if(respone$status_code >= 200 && respone$status_code < 300){
+      message("Grade submission succeeded!")
+    } else {
+      message("Grade submission failed.")
+      message("Press ESC if you want to exit this lesson and you")
+      message("want to try to submit your grade at a later time.")
+      return(FALSE)
+    }
+  }
   TRUE
-}
-
-readline_clean <- function(prompt = "") {
-  wrapped <- strwrap(prompt, width = getOption("width") - 2)
-  mes <- stringr::str_c("| ", wrapped, collapse = "\n")
-  message(mes)
-  readline()
-}
-
-hrule <- function() {
-  message("\n", paste0(rep("#", getOption("width") - 2), collapse = ""), "\n")
 }
